6124e72f3ef615ee7f017f9ddbf461bb6ddaa578
abduljeleelng <abduljeleelng@gmail.com>
2020-04-08 17:28:07 +0100

    Fixed Auth route segmentation

--- src/api/PrivateRoute.js
+++ src/api/PrivateRoute.js
@@ -1,31 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import {BrowserRouter, Route } from "react-router-dom";
 import { isAuthenticated } from "./index";
 import SignUp from '../screen/Signup';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
     // props means components passed down to this pricate route component
-    <Route
-        {...rest}
-        render={props =>
-            isAuthenticated() ? (
-                <Component {...props} />
-            ) : (
-                <SignUp />
-                /*
-                <Redirect
-                    to={{
-                     
-                        
-                        pathname: "/signin",
-                        state: { from: props.location }
-                        
-                    }}
-                />
-                */
-            )
-        }
-    />
+    <BrowserRouter>
+        <Route
+            {...rest}
+            render={props =>
+                isAuthenticated() ? (
+                    <Component {...props} />
+                ) : (
+                    <SignUp />
+                )
+            }
+        />
+    </BrowserRouter>
 );
 
 export default PrivateRoute;
